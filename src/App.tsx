5b5f8c7ebe4393a99c816464d825b3d58c139b9c
das-dipanjan <dipanjandas399@gmail.com>
2026-05-19 17:40:34 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import Journal from "./pages/Journal";
+import Research from "./pages/Research";
+import EditorialBoard from "./pages/EditorialBoard";
+import Contact from "./pages/Contact";
+import SubmitPaper from "./pages/SubmitPaper";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
+  return null;
+};
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/journal" element={<Journal />} />
+          <Route path="/research" element={<Research />} />
+          <Route path="/editorial-board" element={<EditorialBoard />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/submit" element={<SubmitPaper />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
